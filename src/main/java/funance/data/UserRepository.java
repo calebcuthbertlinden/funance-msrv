e0abe56d17b7f676ca0cf611fc444175580f8309
CALEB5 <calebl@discovery.co.za>
2019-05-19 14:59:30 +0200

    [feature] Add user queries and inserts and implement User API

--- src/main/java/funance/data/UserRepository.java
+++ src/main/java/funance/data/UserRepository.java
@@ -1,13 +1,23 @@
 package funance.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    @Query("SELECT u FROM User u WHERE u.username = username")
+    @Query("SELECT u FROM User u WHERE u.username like :username")
     User findByUsername(@Param("username") String username);
+
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO user_entity (id, username, first_name, last_name, email_address, password) VALUES (:identifier, :username, :firstname, :surname, :email, :password)",
+            nativeQuery = true)
+    void createUser(@Param("identifier") int identifier, @Param("username") String username, @Param("firstname") String name, @Param("surname") String surname,
+                    @Param("email") String email, @Param("password") String password);
 }
